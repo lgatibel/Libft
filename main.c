f9631abfe85041670fa60bcc48d004515224ac9b
lgatibel <lionelgatibelza@gmail.com>
2016-06-21 12:57:16 +0200

    21/06/2016 12H56 modfif src/ft_atoi_base

--- main.c
+++ main.c
@@ -5,6 +5,6 @@
 int			main(int ac, char **av)
 {
 	printf("atoi[%d]\n",atoi(av[1]));
-	printf("ft_atoi[%d]\n",ft_atoi_base(av[1], 2));
+	printf("ft_atoi[%d]\n",ft_atoi_base(av[1], ft_atoi(av[2])));
 	return(0);
 }
--- srcs/ft_atoi_base.c
+++ srcs/ft_atoi_base.c
@@ -6,23 +6,41 @@
 /*   By: lgatibel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 16:27:52 by lgatibel          #+#    #+#             */
-/*   Updated: 2016/06/20 14:17:35 by lgatibel         ###   ########.fr       */
+/*   Updated: 2016/06/21 12:53:21 by lgatibel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int		ft_base(char str, int base)
+{
+	static char		tab_maj[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+		'9', 'A', 'B', 'C', 'D', 'E', 'F'};
+	static char		tab_min[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+		'9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	int				i;
+
+	i = -1;
+		while (tab_min[++i] && i < base)
+			if (str == tab_min[i] || str == tab_maj[i])
+				return (i);
+		return (-1);
+}
+
 static int		ft_calc(const char *str, int i, int base)
 {
 	int		n;
 	int		pow;
+	int		tmp;
 
 	n = 0;
 	i -= 1;
 	pow = -1;
-	while (ft_isdigit(str[++i]) && pow < 8)
+	while (ft_isdigit(str[++i]) && ++pow <  32)
 	{
-		n += (ft_power(base, ++pow)) * (str[i] - 48);
+		if((tmp = ft_base(str[i], base)) == -1)
+			break ;
+		n += (ft_power(base, pow)) * tmp;
 	}
 	return (n);
 }
