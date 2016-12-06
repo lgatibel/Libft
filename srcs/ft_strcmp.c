e79bd2082fe8fd351c0b8e2991484510372b4a07
Lionel GATIBELZA <lionelgatibelza@gmail.com>
2016-12-06 17:36:27 +0100

    protected ft_strcmp.c

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: lgatibel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 12:04:13 by lgatibel          #+#    #+#             */
-/*   Updated: 2015/12/11 12:21:20 by lgatibel         ###   ########.fr       */
+/*   Updated: 2016/12/06 17:35:34 by lgatibel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int			ft_strcmp(const char *s1, const char *s2)
 	int	i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (-1);
 	while ((s1[i] && s2[i]) && (s1[i] == s2[i]))
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
